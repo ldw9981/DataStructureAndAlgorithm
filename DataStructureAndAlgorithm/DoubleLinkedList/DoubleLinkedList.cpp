ba8a0300d4661392c2ff0071682cd4b20a1e6357
DongwonLee <ldw9981@gmail.com>
2023-06-17 14:35:05 +0900

    sss

--- DataStructureAndAlgorithm/DoubleLinkedList/DoubleLinkedList.cpp
+++ DataStructureAndAlgorithm/DoubleLinkedList/DoubleLinkedList.cpp
@@ -3,18 +3,117 @@
 
 #include <iostream>
 
+
+struct Node
+{
+	int Data;
+	Node* pNext = nullptr;
+	Node* pPrev = nullptr;
+};
+
+
+struct DoubleLinkedList
+{
+	Node* pHead = nullptr;
+	Node* pTail = nullptr;
+	int Count = 0;
+	void InsertAtTail(int value)
+	{
+		Node* pNewNode = new Node;  // 해제는 delete 메모리주소
+		pNewNode->Data = value;
+		if (pTail == nullptr)
+		{
+			// 아무것도 없는 제일 처음
+			pHead = pNewNode;
+			pTail = pNewNode;
+			Count++;
+			return;
+		}
+
+		pHead->pPrev = pNewNode;
+		pNewNode->pNext = pHead;
+		pHead = pNewNode;
+		Count++;
+	}
+	void PrintListAll()
+	{
+		Node* pNode = pHead;
+		while (pNode != nullptr)
+		{
+			std::cout << pNode->Data << " ";
+			pNode = pNode->pNext;
+		}
+
+		std::cout << std::endl;
+	}
+
+	// 값으로 노드 찾기
+	Node* FindNode(int value)
+	{
+		Node* result = nullptr;
+		//....
+
+		return result;
+	}
+
+	// 노드 삭제하기  
+	void DeleteNode(Node* pNode)
+	{
+		//삭제하려는 노드가 Head,Tail,중간 인 경우 모두 테스트 한다.
+		if (pNode == nullptr)
+		{
+			printf("deleteNode(nullptr)\n");
+			return;
+		}
+
+		if (pNode == pHead)
+		{
+			pHead = pNode->pNext;     // 지우려는 노드가 head이면 다음노드로 head설정
+		}
+		else if (pNode == pTail)
+		{
+			pTail = pNode->pPrev; // 지우려는 노드가 tail이면 이전 노드로 tail설정
+		}
+
+		// 포인터 정리
+		if (pNode->pPrev != nullptr)
+		{
+			pNode->pPrev->pNext = pNode->pNext;
+		}
+
+		if (pNode->pNext != nullptr)
+		{
+			pNode->pNext->pPrev = pNode->pPrev;
+		}
+
+		//할당 해제
+		delete pNode;
+		Count--;
+	}
+
+	// 한번의 순회로 같은 값의 노드를 지우는 함수
+	void Erase(int value)
+	{
+
+	}
+
+	void Clear()
+	{
+		Node* pNodeNext = nullptr;
+		Node* pNode = pHead;
+		while (pNode != nullptr)
+		{
+			pNodeNext = pNode->pNext;
+			DeleteNode(pNode);
+			pNode = pNodeNext;
+		}
+	}
+};
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
