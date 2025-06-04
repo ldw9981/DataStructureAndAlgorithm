082ca54d7c4bf7358be388a2dc1596b213a5bc2c
LeeDongwon <ldw9981@gmail.com>
2025-06-05 06:54:21 +0900

    iterator

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -3,8 +3,6 @@
 
 #include <iostream>
 
-#include <iostream>
-
 template<typename T>
 struct Node {
 	T data;
@@ -13,50 +11,131 @@ struct Node {
 };
 
 template<typename T>
-class DoublyLinkedList {
+class DoublyLinkedList 
+{
 public:
-	Node<T> head, tail; // 더미 노드
+	Node<T> head;
+	Node<T> tail; // 더미 노드
+public:
+	struct Iterator
+	{
+		Node<T>* pCurr = nullptr;
+
+		Iterator(Node<T>* p = nullptr)
+		{
+			pCurr = p;
+		}
+		Iterator& operator++()
+		{
+			pCurr = pCurr->next;
+			return *this;
+		}
+		const T& operator*()
+		{
+			return pCurr->data;
+		}
+		bool operator==(const Iterator& ref)
+		{
+			return  pCurr == ref.pCurr;
+		}
+		bool operator!=(const Iterator& ref)
+		{
+			return  pCurr != ref.pCurr;
+		}
+		void operator=(Node<T>* p)
+		{
+			pCurr = p;
+		}
+
+	}; // struct DoubleLinkedList
+
+
 
 	DoublyLinkedList() {
 		head.next = &tail;
 		tail.prev = &head;
 	}
 
-	void insert_back(int value) {
-		Node* node = new Node<T>{ value, tail.prev, &tail };
+	void insert_back(const T& value) {
+		Node<T>* node = new Node<T>{ value, tail.prev, &tail };
 		tail.prev->next = node;
 		tail.prev = node;
 	}
 
-	void erase(Node<T>* node) {
-		if (node == &head || node == &tail) 
-			return; // 더미는 삭제 금지
-		
+	// 모두 검사
+	void remove(const T& value)
+	{
+		Iterator it = head.next;	
+		do 
+		{
+			if (*it == value)
+			{
+				it = erase(it.pCurr);
+			}			
+			else
+			{
+				++it;
+			}
+			
+		} while (it != end());
+	}
+	Node<T>* erase(Node<T>* node) 
+	{
+		Node<T>* ret = node->next;
 		node->prev->next = node->next;
 		node->next->prev = node->prev;
 		delete node;
+		return ret;
 	}
+	Iterator erase(const Iterator& pos)
+	{
+		Node<T>* ret = pos.pCurr->next;
+		pos.pCurr->prev->next = pos.pCurr->next;
+		pos.pCurr->next->prev = pos.pCurr->prev;
+		delete pos.pCurr;
+		return Iterator(ret);
+	}
+
 
 	void print() {
 		for (Node* cur = head.next; cur != &tail; cur = cur->next)
 			std::cout << cur->data << " ";
 		std::cout << "\n";
 	}
+	Iterator begin()
+	{
+		return Iterator(head.next);
+	}
+	Iterator end()
+	{
+		return Iterator(&tail);
+	}
+	Iterator rbegin()
+	{
+		return Iterator(tail.prev);
+	}
+	Iterator rend()
+	{
+		return Iterator(&head);
+	}
 };
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	DoublyLinkedList<int> m_test;
+	m_test.insert_back(10);
+	m_test.insert_back(20);
+	m_test.insert_back(20);
+	m_test.insert_back(40);
+	m_test.insert_back(50);
+
+	m_test.erase(m_test.rbegin());
+	m_test.remove(20);
+
+	DoublyLinkedList<int>::Iterator it = m_test.begin();
+	for (; it != m_test.end() ; ++it )
+	{
+		std::cout << *it << "\n";
+	}
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
