259194fb972804d56ed6710c9d1e3c71f7d4a364
ldw9981 <ldw9981@gmail.com>
2025-10-15 18:03:55 +0900

    설명 추가

--- BreadthFirstSearch/BreadthFirstSearch.cpp
+++ BreadthFirstSearch/BreadthFirstSearch.cpp
@@ -43,21 +43,22 @@ struct Graph {
 	void PrintBFS(int start)
 	{
 		vector<bool> Visited(Nodes.size(), false); // 방문 안한 노드는 false
-		queue<int> Queue;
-
-
+		queue<int> Queue;	// 방문할 노드 index 를 저장하는 큐
 		Queue.push(start);		// 시작 노드 index 를 큐에 삽입
 
 		while (!Queue.empty())
 		{
-			int current = Queue.front();
-			Queue.pop();
+			int current = Queue.front(); // 큐에서 방문할 노드 index 추출
+			Queue.pop();				// 큐에서 추출한 노드 index 제거
 
 			if (Visited[current] == true) // 방문한 노드면 패스
 				continue;
 
 			Visited[current] = true; // 방문 처리
 			cout << Nodes[current].Data << " ";
+
+			// 현재 노드의 인접 노드들을 큐에 삽입하여 방문 대기
+			// 큐이므로 먼저 삽입된 노드가 먼저 방문됨
 			for (auto& edge : Nodes[current].Edges)
 			{
 				int next = edge.To;
